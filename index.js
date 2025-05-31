4ab469ad37c7ee1a4dddb79489ad50715e03fa39
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-05-31 18:58:42 +0600

    Generated api done

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 // middleWare
-app.use(cors());
+app.use(
+  cors({
+    // Cors setUp cookie
+    origin: ["http://localhost:5173"], // Your frondEnd
+    credentials: true, //Allow Cookies
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sltbrlg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,44 +37,59 @@ async function run() {
       .db("careerCode")
       .collection("application");
 
+    //* JWT Token Related api
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+      const user = { email };
+
+      //* Generated api
+      const token = jwt.sign(user, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "1h",
+      });
+
+      // Set token in the cookies
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      });
+
+      res.send({ success: true });
+    });
+
     // Jobs Api
     // get display all jobs
     app.get("/jobs", async (req, res) => {
-
-      // Query in Email 
+      // Query in Email
       const email = req.query.email;
-      const query = {}
-      if(email){
+      const query = {};
+      if (email) {
         query.hr_email = email;
       }
-
 
       const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // ! explain
+    app.get("/jobs/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = { hr_email: email };
+      const jobs = await jobsCollection.find(query).toArray();
 
-// ! explain
-        app.get("/jobs/applications", async (req,res) => {
-      const email = req.query.email
-      const query = {hr_email: email}
-      const jobs = await jobsCollection.find(query).toArray()
-
-// * bustaparci na  
+      // *
       // should use aggregate to have optimum data fetching
-      for(const job of jobs){
-        const applicationQuery = {jobId: job._id.toString()}
+      for (const job of jobs) {
+        const applicationQuery = { jobId: job._id.toString() };
 
-        const application_count = await applicationsCollection.countDocuments(applicationQuery)
-        job.application_count = application_count
-
+        const application_count = await applicationsCollection.countDocuments(
+          applicationQuery
+        );
+        job.application_count = application_count;
       }
 
-      res.send(jobs)
-    })
-
-
+      res.send(jobs);
+    });
 
     // Job Details
     app.get("/jobs/:id", async (req, res) => {
@@ -76,16 +99,12 @@ async function run() {
       res.send(result);
     });
 
-
     // Add Jobs
-    app.post("/jobs" , async (req,res) => {
+    app.post("/jobs", async (req, res) => {
       const newJob = req.body;
       const result = await jobsCollection.insertOne(newJob);
       res.send(result);
-    })
-
-
-
+    });
 
     app.get("/applications", async (req, res) => {
       const email = req.query.email;
@@ -108,16 +127,13 @@ async function run() {
       res.send(result);
     });
 
-
     // applications Jobs id
-    app.get('/applications/jobs/:id', async (req, res) =>{
+    app.get("/applications/jobs/:id", async (req, res) => {
       const job_id = req.params.jobs_id;
-      const query = {jobId: job_id};
+      const query = { jobId: job_id };
       const result = await applicationsCollection.find(query).toArray();
-      res.send(result)
-    })
-
-
+      res.send(result);
+    });
 
     // jobs application related apis
     app.post("/applications", async (req, res) => {
@@ -126,19 +142,18 @@ async function run() {
       res.send(result);
     });
 
-
-    app.patch("/applications/:id" , async (req,res) => {
+    app.patch("/applications/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const UpdateDoc = {
-        $set:{
-          status:req.body.status
-        }
-      }
+        $set: {
+          status: req.body.status,
+        },
+      };
 
-      const result = await applicationsCollection.updateOne(filter,UpdateDoc);
-      res.send(result)
-    })
+      const result = await applicationsCollection.updateOne(filter, UpdateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
